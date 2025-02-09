910296e8ebd31edc81fc9a4fdf4eb01e98161405
Torben <59419684+entorb@users.noreply.github.com>
2025-02-09 14:37:58 +0100

    Create app_test.py

---
+++ tests/app_test.py
@@ -0,0 +1,28 @@
+"""Use Streamlit AppTest."""
+# ruff: noqa: D100 D103 E402 F841 INP001 N802 PLR2004 S101
+
+import sys
+import warnings
+from pathlib import Path
+
+from streamlit.testing.v1 import AppTest
+
+warnings.filterwarnings("ignore", message=".*streamlit.runtime.scriptrunner_utils.*")
+
+sys.path.insert(0, (Path(__file__).parent.parent / "src").as_posix())
+
+
+def run_and_assert_no_problems(at: AppTest) -> None:
+    at.run(timeout=60)
+    assert not at.exception
+    assert not at.error
+    assert not at.warning
+
+
+def test_app() -> None:
+    p = Path("src/app.py")
+    at = AppTest.from_file(p)
+    run_and_assert_no_problems(at)
+
+    at.session_state["sel_compact"] = True
+    run_and_assert_no_problems(at)
