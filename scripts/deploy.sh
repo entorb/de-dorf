5a81acc1223cb9937c461a96b9fdf1149596179f
Torben <59419684+entorb@users.noreply.github.com>
2025-06-27 21:48:21 +0200

    Add Sentry

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,13 +5,14 @@ echo copying
 # config.toml -> config-prod.toml
 python3 scripts/config_convert.py
 rsync -uz .streamlit/config-prod.toml entorb@entorb.net:streamlit-de-dorf/.streamlit/config.toml
+rsync -uz .streamlit/secrets.toml entorb@entorb.net:streamlit-de-dorf/.streamlit/secrets.toml
 rsync -uz requirements.txt entorb@entorb.net:streamlit-de-dorf/
 rsync -uz Weitere_Zahlen.md entorb@entorb.net:streamlit-de-dorf/
 rsync -ruzv --no-links --delete --delete-excluded --exclude __pycache__ src/ entorb@entorb.net:streamlit-de-dorf/src/
 rsync -ruzv --no-links --delete --delete-excluded data/* entorb@entorb.net:streamlit-de-dorf/data/
 
 echo installing packages
-ssh entorb@entorb.net "pip3.11 install --user streamlit -r streamlit-de-dorf/requirements.txt > /dev/null"
+ssh entorb@entorb.net "pip3.11 install --user -r streamlit-de-dorf/requirements.txt > /dev/null"
 
 echo restarting streamlit-de-dorf
 ssh entorb@entorb.net "supervisorctl restart streamlit-de-dorf"
