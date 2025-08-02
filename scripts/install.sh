4ac7504bc839aaef430dd71167fc29cfca1f4d99
Torben <59419684+entorb@users.noreply.github.com>
2025-08-02 06:07:29 +0200

    update dep

--- scripts/install.sh
+++ scripts/install.sh
@@ -4,6 +4,6 @@ cd $(dirname $0)/..
 pyenv global 3.11
 python -m pip install --upgrade pip
 pip install -r requirements.txt
-pip install -r requirements-dev.txt
+pip install --upgrade -r requirements-dev.txt
 pip freeze >requirements-all.txt
 pyenv global 3.13
