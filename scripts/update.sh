c61f2326bd1798f76947023f7fec10a5b89d16e5
Torben <59419684+entorb@users.noreply.github.com>
2025-10-19 00:10:29 +0200

    pinning numpy and pyarrow

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
 cd $(dirname $0)/..
 
-uv remove streamlit
+uv remove numpy pandas pyarrow streamlit
 uv remove --dev ruff pre-commit pytest pytest-cov tomli-w watchdog
 
 uv lock --upgrade
 uv sync --upgrade
 
-uv add streamlit
+uv add numpy==2.2.3 pandas==2.2.3 pyarrow==20.0.0 streamlit
 uv add --dev ruff pre-commit pytest pytest-cov tomli-w watchdog
 
 uv lock --upgrade
 uv sync --upgrade
+python scripts/gen_requirements.py
