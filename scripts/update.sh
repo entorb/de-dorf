c3108a0cc7a61423d92509f17a5ae9dc3cdb592a
Torben <59419684+entorb@users.noreply.github.com>
2026-04-12 16:33:26 +0200

    Tools

---
+++ scripts/update.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+# ensure we are in the root dir
+cd $(dirname $0)/..
+
+# exit upon error
+set -e
+
+uv remove numpy pandas pyarrow streamlit sentry-sdk
+uv remove --dev ruff pre-commit pytest pytest-cov tomli-w
+
+uv lock --upgrade
+uv sync --upgrade
+
+# pin to old versions due to Uberspace restrictions
+uv add numpy==2.2.3 pandas==2.2.3 pyarrow==20.0.0 streamlit sentry-sdk
+uv add --dev ruff pre-commit pytest pytest-cov tomli-w
+
+uv lock --upgrade
+uv sync --upgrade
+python scripts/gen_requirements.py
+
+# ruff
+uv run ruff format
+uv run ruff check --fix
+
+# pre-commit
+uv run pre-commit autoupdate
+uv run pre-commit run --all-files
+
+echo DONE
