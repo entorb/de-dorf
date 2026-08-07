58d1270f858cc2169466bb6604133d85a90c991f
Torben <59419684+entorb@users.noreply.github.com>
2026-08-07 22:26:47 +0200

    pre-commit to prek

--- scripts/update.sh
+++ scripts/update.sh
@@ -7,14 +7,14 @@ cd $(dirname $0)/..
 set -e
 
 uv remove numpy pandas pyarrow streamlit sentry-sdk
-uv remove --dev ruff pre-commit pytest pytest-cov tomli-w
+uv remove --dev ruff pytest pytest-cov tomli-w
 
 uv lock --upgrade
 uv sync --upgrade
 
 # pin to old versions due to Uberspace restrictions
 uv add numpy==2.2.3 pandas==2.2.3 pyarrow==20.0.0 streamlit sentry-sdk
-uv add --dev ruff pre-commit pytest pytest-cov tomli-w
+uv add --dev ruff pytest pytest-cov tomli-w
 
 uv lock --upgrade
 uv sync --upgrade
@@ -25,7 +25,7 @@ uv run ruff format
 uv run ruff check --fix
 
 # pre-commit
-uv run pre-commit autoupdate
-uv run pre-commit run --all-files
+prek autoupdate
+prek run --all-files
 
 echo DONE
